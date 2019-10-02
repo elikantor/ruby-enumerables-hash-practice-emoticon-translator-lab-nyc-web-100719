a3403664c05bc82c47df6e83de2abb6a9d44277e
Eli Kantor <eli.kantor@outlook.com>
2019-10-02 21:00:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
-# require modules here
+require 'pry'
 
-def load_library
-  # code goes here
+require "yaml"
+
+
+def load_library(emoticons = YAML.load_file('emoticons.yml'))
+  emoticons = {}
+  
+  emoticons.map {|ele, subele|
+    binding.pry
+    emoticons[:get_meaning] = {}
+    emoticons[:get_meaning] = ele
+    emoticons[:get_emoticon] = {}
+    emoticons[:get_emoticon] = subele
+  }
+  
+  emoticons
 end
 
 def get_japanese_emoticon
