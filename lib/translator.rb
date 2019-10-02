c9869f0aacfe4e7b163e86f3ca334ad07881c0d9
Eli Kantor <eli.kantor@outlook.com>
2019-10-02 21:16:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require "yaml"
 
 def load_library(asdf)
   emoticons = YAML.load_file(asdf)
-  
+  new_hash = {}
   emoticons.map {|ele, subele|
     # binding.pry
     emoticons[:get_meaning] = {}
@@ -14,7 +14,7 @@ def load_library(asdf)
     emoticons[:get_emoticon] = subele
   }
   
-  emoticons
+  new_hash
 end
 
 def get_japanese_emoticon
